406746b1db6e473c626799dcaa21e96915eb3cbd
Nouran Elshazly <nouranelshazly19@gmail.com>
2024-10-26 00:44:25 +0300

    coding for kids

---
+++ Python_Basics/Data_types&Variables/Data_types.py
@@ -0,0 +1,29 @@
+# In this section, we will discuss data types and variables. First, there are some rules for naming variables: 
+
+# 1. A variable cannot start with a number.
+# 2. Spaces are not allowed in variable names; for example, "first name" would result in an error.
+# 3. Instead, you can write it as "firstname" without any spaces or use an underscore, like "first_name."
+
+# We use variables to store different data types.
+
+#there are a different data type : 1- integer (int)
+number = 9 #===> number is a variable with store an integer data (9) 
+print(number) #===> we use print function to print anything for user
+#if we want to know any variable type we use :
+print(type(number))
+#2- float 
+number2=9.1 #===> any dicimal number is float 
+print(number2)
+print(type(number2))
+
+#3- string  (anything  " between double cotuchin ")
+first_name = "Nouran"
+Last_name = "Elshazly"
+print(first_name)
+print(Last_name)
+print(type(first_name))
+print(type(Last_name))
+#4- boolean => true or false 
+x=9>10  #=> x return false so it is an boolean variable
+print (x) #=> false 
+
